03cbcf81283a4e0288afa117f23b0811c1bd6cd1
danielsarj <dnsarj@gmail.com>
2024-10-28 13:28:10 -0500

    add jobmode flag

--- 01_alignment/03_cellranger_count.R
+++ 01_alignment/03_cellranger_count.R
@@ -7,7 +7,7 @@ conditions <- c('NI', 'RV', 'IVA')
 
 # sbtach file topper
 sbatch_topper <- '#!/bin/sh\n' %&% '#SBATCH --time=36:00:00\n' %&% 
-  '#SBATCH --mem=180G\n#SBATCH --nodes=1\n#SBATCH --ntasks-per-node=20\n' %&% '#SBATCH --partition=caslake\n' %&% 
+  '#SBATCH --mem=64G\n' %&% '#SBATCH --partition=caslake\n' %&% 
   '#SBATCH --account=pi-lbarreiro\n'
 
 for (b in 1:length(batches)){
@@ -18,7 +18,7 @@ for (b in 1:length(batches)){
       ' --fastqs=/project/lbarreiro/USERS/jbatista/asthma_project/20240206_LH00315_0085_A22H7FKLT3-LB-MP-2s-Astma-pl1-4lns/FastX/,'%&%
       '/project/lbarreiro/USERS/jbatista/asthma_project/20240213_LH00315_0092_A22HGWMLT3-LB-MP-2s-Astma-pl1-8lns/FastX '%&%
       '--sample=LB-MP-pl1-Asthma-'%&% batches[b] %&%'-'%&% conditions[c] %&%
-      ' --transcriptome=/project/lbarreiro/USERS/daniel/asthma_project/alignment/GRCh38_with_IAV_and_RV'
+      ' --transcriptome=/project/lbarreiro/USERS/daniel/asthma_project/alignment/GRCh38_with_IAV_and_RV --jobmode=slurm'
 
     # create sbatch file
     cat(sbatch_topper%&%'\n\n'%&%command_line, 
