b2184edfdb7b30dbe786b5b6b2e24fbdd8709b8f
danielsarj <dnsarj@gmail.com>
2024-10-29 12:43:16 -0500

    rename chr in vcf

--- 02_demultiplex/01_genotypeQC.sh
+++ 02_demultiplex/01_genotypeQC.sh
@@ -1,12 +1,14 @@
 module load plink
+cd  /project/lbarreiro/USERS/daniel/asthma_project/genotypes
 
 # convert vcf to plink format
-plink --allow-extra-chr --chr 1-22 --keep-allele-order --vcf-idspace-to - --make-bed --out /project/lbarreiro/USERS/daniel/asthma_project/genotypes/all_genotypes --vcf /project/lbarreiro/USERS/daniel/asthma_project/genotypes/all_genotypes.GRCh38.vcf.gz
+plink --allow-extra-chr --chr 1-22 --keep-allele-order --vcf-idspace-to - --make-bed --out all_genotypes --vcf all_genotypes.GRCh38.vcf.gz
 
 # perform genotype QC 
-plink --bfile /project/lbarreiro/USERS/daniel/asthma_project/genotypes/all_genotypes --geno 0.1 --maf 0.05 --hwe 1e-6 --make-bed --out /project/lbarreiro/USERS/daniel/asthma_project/genotypes/filtered_genotypes
+plink --bfile all_genotypes --geno 0.1 --maf 0.05 --hwe 1e-6 --make-bed --out filtered_genotypes
 
 # back to vcf
-plink --bfile /project/lbarreiro/USERS/daniel/asthma_project/genotypes/filtered_genotypes --keep-allele-order --recode vcf --out /project/lbarreiro/USERS/daniel/asthma_project/genotypes/filtered_genotypes
+plink --bfile filtered_genotypes --keep-allele-order --recode vcf --out filtered_genotypes
 
-gzip project/lbarreiro/USERS/daniel/asthma_project/genotypes/*.vcf
+# rename chr 
+/project/lbarreiro/USERS/daniel/SOFTWARE/bcftools-1.19/bcftools annotate --rename-chrs chr_names.txt filtered_genotypes.vcf -Oz -o filtr_renamed_genotypes.vcf
