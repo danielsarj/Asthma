5b7ff73b68c49f81cbe745c5278a582d8a5684be
danielsarj <dnsarj@gmail.com>
2024-11-04 12:31:56 -0600

    make barplot of demuxalots results

---
+++ 02_demultiplex/04_plot_summary.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(data.table)
+library(janitor)
+library(forcats)
+"%&%" <- function(a,b) paste(a,b, sep = "")
+setwd('/project/lbarreiro/USERS/daniel/asthma_project/alignment')
+
+batches <- c('B1','B2','B3','B4')
+conditions <- c('NI', 'RV', 'IVA')
+
+for (b in 1:length(batches)){
+  for (c in 1:length(conditions)){
+    f <- fread(batches[b] %&%'-' %&% conditions[c] %&%'_GRCh38/demuxalot/demuxalot_summary.tsv') %>% 
+      mutate(batch=batches[b], condition=conditions[c]) %>% clean_names()
+    if (exists('summary_df')){
+      summary_df <- rbind(summary_df, f)
+    } else {summary_df <- f}
+  }
+}
+summary_df$classification <- gsub('\\+', '', summary_df$classification)
+
+summary_df <- summary_df %>% group_by(classification, batch, condition) %>% 
+  summarise('n_assigs'=sum(assignment_n))
+
+summary_df %>% filter(classification %in% c('doublet', '')==F) %>%
+  ggplot(.) + geom_col(aes(x=fct_reorder(classification, batch), y=n_assigs, fill=batch)) + coord_flip() +
+  facet_wrap(~condition)
+
+ggsave('demultiplexing_summary.pdf', height=6, width=8)
