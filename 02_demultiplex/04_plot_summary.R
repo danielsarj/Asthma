6691db45ed4e11293e6e0a32a89f535b6382870f
danielsarj <dnsarj@gmail.com>
2024-11-12 10:44:51 -0600

    plot doublets

--- 02_demultiplex/04_plot_summary.R
+++ 02_demultiplex/04_plot_summary.R
@@ -22,8 +22,15 @@ summary_df$classification <- gsub('\\+', '', summary_df$classification)
 summary_df <- summary_df %>% group_by(classification, batch, condition) %>% 
   summarise('n_assigs'=sum(assignment_n))
 
-summary_df %>% filter(classification %in% c('doublet', '')==F) %>%
+summary_df %>% filter(classification %in% c('doublet')==F) %>%
   ggplot(.) + geom_col(aes(x=fct_reorder(classification, batch), y=n_assigs, fill=batch)) + coord_flip() +
   facet_wrap(~condition)
 
 ggsave('demultiplexing_summary.pdf', height=6, width=8)
+
+summary_df %>% 
+  ggplot(.) + geom_col(aes(x=fct_reorder(classification, batch), y=n_assigs, fill=batch)) + coord_flip() +
+  facet_wrap(~condition) + 
+  scale_y_continuous(breaks = seq(0,16000, by=2000))
+
+ggsave('demultiplexing_summary_wdoublet.pdf', height=6, width=10)
