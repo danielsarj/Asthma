1f9b3f21321c51eda354e3a0b8a93db128b7dad9
danielsarj <dnsarj@gmail.com>
2025-01-11 10:32:52 -0600

    annotate celltypes using Azimuth PMCF ref

--- 03_scRNAanalysis/03_annotate_celltypes.R
+++ 03_scRNAanalysis/03_annotate_celltypes.R
@@ -15,18 +15,18 @@ for (i in 1:length(files)){
   
   objs <- readRDS(conditions[i] %&%'_allbatches.scaled.clustered.rds')
   DefaultAssay(objs) <- 'RNA'
-  
+
   # annotate celltypes using a PBMC reference and filter cells by mapping.score > 50%
   print(objs)
   objs <- RunAzimuth(objs, reference='pbmcref', verbose=FALSE)
   objs <- objs %>% subset(subset=mapping.score>0.5)
   print(objs)
-  saveRDS(objs, file=conditions[i] %&%'_allbatches.scaled.clustered.celltypeannotation.rds')
+  saveRDS(objs, file=conditions[i] %&%'.scaled.clustered.celltypeannotation.rds')
   
   # save UMAP viz
   umaps[[i]] <- DimPlot(objs, group.by='predicted.celltype.l1', label=TRUE, label.size=5) + 
     ggtitle(conditions[i])
 }
-
+  
 umaps[[1]] | umaps[[2]] | umaps[[3]]
 ggsave(filename='UMAP_celltypes.allconditions.pdf', height=6, width=18)
