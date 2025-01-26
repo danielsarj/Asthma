7ba91656dafd7e332becb5ff637f9a8fd18c963b
danielsarj <dnsarj@gmail.com>
2025-01-25 19:09:22 -0600

    integrate pairs of conditions separately

---
+++ 03_scRNAanalysis/02a_integrate_NI_RV.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(Seurat)
+library(patchwork)
+library(glmGamPoi)
+library(future)
+plan('multicore', workers=4)
+"%&%" <- function(a,b) paste(a,b, sep = "")
+options(future.globals.maxSize=200*1024^3)
+setwd('/project/lbarreiro/USERS/daniel/asthma_project/scRNAanalysis')
+
+conditions <- c('NI','RV')
+seurat_objs <- list()
+
+# read each file
+for (c in 1:length(conditions)){
+  seurat_objs <- append(seurat_objs, readRDS(conditions[c]%&%'_raw.objects.batches.rds'))
+}
+
+# SCTransform each batch/condition
+for (i in 1:length(seurat_objs)){
+  seurat_objs[[i]] <- seurat_objs[[i]] %>% SCTransform(vars.to.regress=c('percent.mt'), 
+                                                       conserve.memory=TRUE, method='glmGamPoi')
+}
+
+# select features for integration
+features <- SelectIntegrationFeatures(object.list=seurat_objs, nfeatures=3000)
+
+# prepare for SCT integration
+seurat_objs <- PrepSCTIntegration(object.list=seurat_objs, anchor.features=features)
+
+# find anchors
+anchors <- FindIntegrationAnchors(object.list=seurat_objs, normalization.method='SCT', 
+                                  anchor.features=features)
+
+# integrate data
+integrated_obj <- IntegrateData(anchorset=anchors, normalization.method='SCT')
+
+# PCA, clustering, viz
+integrated_obj <- integrated_obj %>% RunPCA(npcs=30) %>% FindNeighbors(dims=1:30) %>% 
+  FindClusters() %>% RunUMAP(dims=1:30)
+DimPlot(integrated_obj, reduction='umap', group.by=c('batch', 'condition'))
+ggsave(filename='UMAP_QCintegratedNI_RV.pdf', height=6, width=13)
+
+DimPlot(integrated_obj, reduction='umap', group.by='batch', split.by='condition')
+ggsave(filename='UMAP_QCintegratedNI_RV_splitcondition.pdf', height=6, width=18)
+
+# save list of integrated batches/ditions
+saveRDS(integrated_obj, file='NI_RV_integrated.rds')
