6b865d5db84287ebab1bff089155684c89c90591
danielsarj <dnsarj@gmail.com>
2025-01-27 11:12:56 -0600

    fix typo

--- 03_scRNAanalysis/03_annotate_celltypes.R
+++ 03_scRNAanalysis/03_annotate_celltypes.R
@@ -28,23 +28,25 @@ for (i in 1:length(conditions)){
   # save UMAP viz
   DimPlot(objs, reduction='umap', group.by='predicted.celltype.l1', split.by='condition',
           label=TRUE, label.size=5, repel=TRUE)
-  ggsave(filename='UMAP_'%&%conditions[i]%&%'_celltypes.pdf', height=6, width=18)
+  ggsave(filename='UMAP_'%&%conditions[i]%&%'_celltypes.pdf', height=6, width=12)
   
   # analyze metadata
-  metadata <- objs@meta.data %>% 
+  metadata <- objs@metadata %>% 
     filter(predicted.celltype.l1 %in% c('other', 'other T')==FALSE)
+  second_level <- sub('.*_', '', conditions[i])
+  metadata$condition <- factor(metadata$condition, levels=c('NI', second_level))
   
   ## proportion of celltype per condition
   summ_condition <- metadata %>% select(condition, predicted.celltype.l1) %>%
     group_by(condition, predicted.celltype.l1) %>% summarise(n=n())
   ggplot(summ_condition) + geom_col(aes(x=condition, y=n, fill=predicted.celltype.l1), position='fill') +
     theme_bw()
-  ggsave(filename='BarPlot_'%&%conditions[i]%&%'_proportion.celltypes.perCond.pdf', height=6, width=8)
+  ggsave(filename='BarPlot_'%&% conditions[i]%&%'_proportion.celltypes.perCond.pdf', height=6, width=8)
   
   ## proportion of celltype per indv. and condition
   summ_indv_condition <- metadata %>% select(condition, IDs, predicted.celltype.l1) %>%
     group_by(condition, IDs, predicted.celltype.l1) %>% summarise(n=n())
   ggplot(summ_indv_condition) + geom_col(aes(x=IDs, y=n, fill=predicted.celltype.l1), position='fill') +
     facet_wrap(~condition) + theme_bw() + coord_flip()
-  ggsave(filename='BarPlot_'%&%conditions[i]%&%'_proportion.celltypes.perIDandCond.pdf', height=10, width=14)
+  ggsave(filename='BarPlot_'%&% conditions[i]%&%'_proportion.celltypes.perIDandCond.pdf', height=10, width=14)
 }
