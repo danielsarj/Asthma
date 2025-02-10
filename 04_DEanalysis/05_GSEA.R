107ae885aa2bc9fa4e3d16b99c60259b57424788
danielsarj <dnsarj@gmail.com>
2025-02-10 13:32:42 -0600

    perform GSEA using hallmark pathways

---
+++ 04_DEanalysis/05_GSEA.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(data.table)
+library(fgsea)
+library(msigdbr)
+"%&%" <- function(a,b) paste(a,b, sep = "")
+setwd('/project/lbarreiro/USERS/daniel/asthma_project/DEanalysis')
+conditions <- c('RV', 'IVA')
+cells_seurat <- c('B','CD4-T','CD8-T','Mono','NK')
+interactions <- c('none','asthma', 'income')
+
+# retrieve human genes for the hallmark collection gene sets
+human.path.list <- msigdbr(species='Homo sapiens', category='H') %>% 
+  split(x=.$gene_symbol, f=.$gs_name)
+
+for (int in interactions){
+  
+  # load DE limma results
+  if (int=='none'){
+    DE_results <- fread('NI_IVAxRV_limma_results_avglogCPM.filtered.txt') %>% rename(gene=Gene)
+  } else {
+  DE_results <- fread('NI_IVAxRV_'%&%int%&%'_limma_results_avglogCPM.filtered.txt')
+  }
+  
+  for (i in 1:length(conditions)){
+    for (ctype in cells_seurat){
+      
+      # subset based on celltype and condition
+      subset_DE_results <- DE_results %>% filter(celltype==ctype, condition==conditions[i]) 
+      
+      # format values for fGSEA 
+      if (int=='income'){
+        subset_DE_results <- subset_DE_results %>% group_by(gene) %>% slice_min(adj.P.Val) %>% slice_max(abs(t))
+        subset_DE_results <- subset_DE_results %>% arrange(t) %>% select(gene, t)
+        subset_DE_results <- setNames(subset_DE_results$t, subset_DE_results$gene)
+      } else {
+        subset_DE_results <- subset_DE_results %>% arrange(t) %>% select(gene, t)
+        subset_DE_results <- setNames(subset_DE_results$t, subset_DE_results$gene)
+      }
+      
+      # run fGSEA
+      fgseaRes <- fgsea(pathways=human.path.list,
+                        stats=subset_DE_results, 
+                        minSize=15, 
+                        maxSize=1000)
+      fgseaRes <- fgseaRes %>% mutate(celltype=ctype, condition=conditions[i], interaction=int) %>% 
+        select('pathway','pval','padj','log2err','ES','NES','size','celltype','condition','interaction','leadingEdge')
+      
+      # compile results
+      if (exists('compiled.fgseaRes')){
+        compiled.fgseaRes <- rbind(compiled.fgseaRes, fgseaRes)
+      } else {
+        compiled.fgseaRes <- fgseaRes
+      }
+    }
+  }
+}
+fwrite(compiled.fgseaRes, 'NI_IVAxRV_GSEAresults.txt', sep=' ', na='NA', col.names=T)
+
+# plot number of enriched pathways per celltype/condition/interaction
+summ <- compiled.fgseaRes %>% group_by(celltype, condition, interaction) %>% filter(padj<0.05) %>%
+  summarise(n=n())
+ggplot(summ) + geom_col(aes(x=celltype, y=n)) + theme_bw() + 
+  facet_grid(cols=vars(interaction), rows=vars(condition))
+ggsave('NI_IVAxRV_SigGeneSets_acrossInteractions.pdf', height=4, width=8)
