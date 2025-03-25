b2603deb8be742555ef59457857a270d654953bb
danielsarj <dnsarj@gmail.com>
2025-03-24 20:16:25 -0500

    set up permutations

--- 05_QTL_mapping/03a_runMatrixEQTL.R
+++ 05_QTL_mapping/03a_runMatrixEQTL.R
@@ -39,23 +39,58 @@ exp_d$CreateFromMatrix(exp_matrix_mat)
 rm(exp_matrix, exp_matrix_mat)
 
 # run main MatrixeQTL function
-me <- Matrix_eQTL_main(
-  snps = snp_d,
-  gene = exp_d,
-  pvOutputThreshold = 0,
-  useModel = modelLINEAR,
-  errorCovariance = numeric(),
-  verbose = TRUE,
-  pvOutputThreshold.cis = 1,
-  snpspos = snp_local,
-  genepos = gene_local,
-  cisDist = 1e6,
-  pvalue.hist = TRUE,
-  min.pv.by.genesnp = FALSE,
-  noFDRsaveMemory = FALSE)
-
-# retrieve and save results
-cis_qtls <- me$cis$eqtls %>% mutate(condition=args$cond, celltype=args$ctype, SE=abs(beta/qnorm(pvalue/2)))
-cis_qtls <- inner_join(cis_qtls, snp_local, by=c('snps'='snpid')) %>% 
-  select(snps, chr, pos, gene, statistic, pvalue, FDR, beta, SE, condition, celltype) %>% arrange(chr, pos)
-fwrite(cis_qtls, 'matrixEQTL_results/'%&%args$cond%&%'_'%&%args$ctype%&%'_elbowPCs_cisQTL_sumstats.txt', quote=F, sep='\t', na='NA')
+n_permutations <- 10
+for (i in 1:n_permutations+1){
+  print(i)
+  if (i==1){
+    # QTL mapping without permutation of gene expression labels
+    me <- Matrix_eQTL_main(
+      snps = snp_d,
+      gene = exp_d,
+      pvOutputThreshold = 0,
+      useModel = modelLINEAR,
+      errorCovariance = numeric(),
+      verbose = FALSE,
+      pvOutputThreshold.cis = 1,
+      snpspos = snp_local,
+      genepos = gene_local,
+      cisDist = 1e6,
+      pvalue.hist = TRUE,
+      min.pv.by.genesnp = FALSE,
+      noFDRsaveMemory = FALSE)
+    
+    # retrieve and save results
+    cis_qtls <- me$cis$eqtls %>% mutate(condition=args$cond, celltype=args$ctype, SE=abs(beta/qnorm(pvalue/2)))
+    cis_qtls <- inner_join(cis_qtls, snp_local, by=c('snps'='snpid')) %>% 
+      select(snps, chr, pos, gene, statistic, pvalue, FDR, beta, SE, condition, celltype) %>% arrange(chr, pos)
+    fwrite(cis_qtls, 'matrixEQTL_results/'%&%args$cond%&%'_'%&%args$ctype%&%'_elbowPCs_cisQTL_sumstats.txt', quote=F, sep='\t', na='NA')
+    
+  } else {
+    
+    # reorder expression data frame
+    permuted_gene <- exp_d$Clone()
+    permuted_gene$ColumnSubsample( sample(1:permuted_gene$nCols(), permuted_gene$nCols()) )
+    
+    # QTL mapping with permuted expression labels
+    me <- Matrix_eQTL_main(
+      snps = snp_d,
+      gene = permuted_gene,
+      pvOutputThreshold = 0,
+      useModel = modelLINEAR,
+      errorCovariance = numeric(),
+      verbose = FALSE,
+      pvOutputThreshold.cis = 1,
+      snpspos = snp_local,
+      genepos = gene_local,
+      cisDist = 1e6,
+      pvalue.hist = TRUE,
+      min.pv.by.genesnp = FALSE,
+      noFDRsaveMemory = FALSE)
+    
+    # retrieve and save results
+    cis_qtls <- me$cis$eqtls %>% mutate(condition=args$cond, celltype=args$ctype, SE=abs(beta/qnorm(pvalue/2)))
+    cis_qtls <- inner_join(cis_qtls, snp_local, by=c('snps'='snpid')) %>% 
+      select(snps, chr, pos, gene, statistic, pvalue, FDR, beta, SE, condition, celltype) %>% arrange(chr, pos)
+    fwrite(cis_qtls, 'matrixEQTL_results/'%&%args$cond%&%'_'%&%args$ctype%&%'_Perm'%&%i-1%&%'_elbowPCs_cisQTL_sumstats.txt', quote=F, sep='\t', na='NA')
+  }
+}
