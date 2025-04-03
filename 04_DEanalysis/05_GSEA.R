7a430aaeb0d93e0bcc6a728aec10af92e62af301
danielsarj <dnsarj@gmail.com>
2025-04-03 16:13:51 -0500

    make bubbleplot for sig gsea results

--- 04_DEanalysis/05_GSEA.R
+++ 04_DEanalysis/05_GSEA.R
@@ -9,7 +9,7 @@ cells_seurat <- c('B','CD4-T','CD8-T','Mono','NK')
 interactions <- c('none','asthma', 'income')
 
 # retrieve human genes for the hallmark collection gene sets
-human.path.list <- msigdbr(species='Homo sapiens', category='H') %>% 
+human.path.list <- msigdbr(species='Homo sapiens', collection='H') %>% 
   split(x=.$gene_symbol, f=.$gs_name)
 
 for (int in interactions){
@@ -27,8 +27,8 @@ for (int in interactions){
       subset_DE_results <- DE_results %>% filter(celltype==ctype, condition==conditions[i]) 
       
       # format values for fGSEA
-        subset_DE_results <- subset_DE_results %>% arrange(t) %>% select(gene, t)
-        subset_DE_results <- setNames(subset_DE_results$t, subset_DE_results$gene)
+      subset_DE_results <- subset_DE_results %>% arrange(t) %>% select(gene, t)
+      subset_DE_results <- setNames(subset_DE_results$t, subset_DE_results$gene)
       
       # run fGSEA
       fgseaRes <- fgsea(pathways=human.path.list,
@@ -55,3 +55,15 @@ summ <- compiled.fgseaRes %>% group_by(celltype, condition, interaction) %>% fil
 ggplot(summ) + geom_col(aes(x=celltype, y=n)) + theme_bw() + 
   facet_grid(cols=vars(interaction), rows=vars(condition))
 ggsave('NI_IVAxRV_SigGeneSets_acrossInteractions.pdf', height=4, width=8)
+
+# bubble plots!
+
+# Bubble plot
+for (int in interactions){
+  compiled.fgseaRes %>% filter(padj<0.05, abs(NES)>=1.5, interaction==int) %>% 
+    ggplot(., aes(x=NES, y=reorder(pathway, NES), size=size, color=-log10(padj))) +
+    geom_point(alpha=0.8) + scale_size(range=c(1,10)) + scale_color_gradient(low='blue', high='red') +  
+    labs(x='Normalized Enrichment Score (NES)', y='Pathway', size='Gene Count', color='-log10(FDR)') +
+    theme_bw() + facet_grid(cols=vars(celltype), rows=vars(condition))
+  ggsave('NI_IVAxRV_SigGeneSets_'%&%int%&%'_bubbleplot.pdf', height=8, width=12)
+}
