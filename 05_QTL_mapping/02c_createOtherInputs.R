5cb06464b272c37e0eab417ecc9e61e5bf1e5872
danielsarj <dnsarj@gmail.com>
2025-04-11 14:57:17 -0500

    change how SNPs are coded in the position file

--- 05_QTL_mapping/02c_createOtherInputs.R
+++ 05_QTL_mapping/02c_createOtherInputs.R
@@ -4,9 +4,16 @@ library(janitor)
 "%&%" <- function(a,b) paste(a,b, sep = "")
 setwd('/project/lbarreiro/USERS/daniel/asthma_project/QTLmapping')
 
+# create dosage file
+dosage <- fread('../genotypes/imputed_vcfs/imputed_dosage.raw') %>% 
+  select(-c(FID, PAT, MAT, SEX, PHENOTYPE)) %>% t() %>% as.data.frame() %>% 
+  rownames_to_column() %>% row_to_names(row_number=1)
+colnames(dosage)[1] <- c('snpid')
+fwrite(dosage, '../genotypes/imputed_vcfs/imputed_dosage.txt', col.names=T, sep='\t')
+
 # create SNP location file
-snp_local <- fread('../genotypes/imputed_vcfs/filtered_merged.bim') %>% select(V2, V1, V4)
-colnames(snp_local) <- c('snpid', 'chr', 'pos')
+snp_local <- fread('../genotypes/imputed_vcfs/filtered_merged.bim') %>% select(V2, V1, V4, V5) %>%
+  mutate(snpid=V2%&%'_'%&%V5) %>% rename(chr=V1, pos=V4) %>% select(snpid, chr, pos)
 fwrite(snp_local, '../genotypes/imputed_vcfs/snp_location.txt', col.names=T, sep='\t')
 
 # create gene location file
@@ -18,17 +25,3 @@ gene_local$hgnc_symbol <- gene_local$hgnc_symbol %>% na_if('')
 gene_local <- gene_local %>% drop_na()
 colnames(gene_local) <- c('geneid', 'chr', 'left', 'right')
 fwrite(gene_local, 'gene_location.txt', col.names=T, sep='\t')
-
-# create dosage file
-dosage <- fread('../genotypes/imputed_vcfs/imputed_dosage.raw') %>% 
-  select(-c(FID, PAT, MAT, SEX, PHENOTYPE)) %>% t() %>% as.data.frame() %>% 
-  rownames_to_column() %>% row_to_names(row_number=1)
-tmp_colnames <- colnames(dosage)
-tmp_colnames <- gsub('SEA3', 'SEA-3', tmp_colnames)
-colnames(dosage) <- tmp_colnames
-dosage$IID <- gsub('_A', '', dosage$IID)
-dosage$IID <- gsub('_T', '', dosage$IID)
-dosage$IID <- gsub('_C', '', dosage$IID)
-dosage$IID <- gsub('_G', '', dosage$IID)
-colnames(dosage)[1] <- c('snpid')
-fwrite(dosage, '../genotypes/imputed_vcfs/imputed_dosage.txt', col.names=T, sep='\t')
