caa63662860f7afe74daeac33e63d297fd420e48
danielsarj <dnsarj@gmail.com>
2025-04-11 18:06:40 -0500

    replace fdr by pval in volcano plots

--- 04_DEanalysis/04_summarise_DE_interaction_results.R
+++ 04_DEanalysis/04_summarise_DE_interaction_results.R
@@ -43,9 +43,9 @@ for (int in interactions){
         results$adj.P.Val <- p.adjust(results$P.Value, method='BH', n=nrow(results))
 
         # volcano plot
-        ggplot(results) + geom_point(aes(logFC, -log10(adj.P.Val)), size=0.5, alpha=0.5) +
-          theme_bw() + ylab('-log10(FDR)') + ggtitle(conditions[i]%&%' - '%&%ctype) +
-          geom_text_repel(aes(logFC, -log10(adj.P.Val), label=ifelse(adj.P.Val<0.05,gene, '')), 
+        ggplot(results) + geom_point(aes(logFC, -log10(P.Value)), size=0.5, alpha=0.5) +
+          theme_bw() + ylab('-log10(pvalue)') + ggtitle(conditions[i]%&%' - '%&%ctype) +
+          geom_text_repel(aes(logFC, -log10(P.Value), label=ifelse(P.Value<0.00001,gene, '')), 
                       colour='red', size=3) 
         ggsave('NI_'%&%conditions[i]%&%'_'%&%int%&%'_limma_'%&%ctype%&%'_volcanoplot.pdf', height=3, width=4)
     
@@ -58,8 +58,8 @@ for (int in interactions){
   }
   fwrite(full_results, 'NI_IVAxRV_'%&%int%&%'_limma_results_avglogCPM.filtered.txt', sep=' ')
   
-  ggplot(full_results) + geom_point(aes(logFC, -log10(adj.P.Val)), size=0.5, alpha=0.5) +
-    theme_bw() + ylab('-log10(FDR)') + facet_grid(cols=vars(celltype), rows=vars(condition)) +
+  ggplot(full_results) + geom_point(aes(logFC, -log10(P.Value)), size=0.5, alpha=0.5) +
+    theme_bw() + ylab('-log10(pvalue)') + facet_grid(cols=vars(celltype), rows=vars(condition)) +
     geom_hline(yintercept=1.30103, color='red')
   ggsave('NI_IVAxRV_'%&%int%&%'_limma_facetgrid_volcanoplot.pdf', height=5, width=8)
   rm(full_results)
