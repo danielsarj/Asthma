d03f4c76a5c451864987718b276d4fd777bc3b58
danielsarj <dnsarj@gmail.com>
2025-04-16 06:55:43 -0500

    fix qqplot

--- 05_QTL_mapping/03c_compileMatrixEQTLresults.R
+++ 05_QTL_mapping/03c_compileMatrixEQTLresults.R
@@ -32,11 +32,6 @@ for (cond in conditions){
       } else {
         tmp <- fread(cond%&%'_'%&%ctype%&%'_Perm'%&%perm%&%'_elbowPCs_cisQTL_sumstats.txt') 
         
-        #histogram of unadjusted pvalues
-        pdf('plots/'%&%cond%&%'_'%&%ctype%&%'_Perm'%&%perm%&%'_cisQTL_pvalhistogram.pdf', width=4, height=4)
-        hist(tmp$pvalue, main = cond%&%' '%&%ctype%&%' Perm'%&%perm%&%' cisQTL pvalues', breaks=100)
-        dev.off()
-        
         # select top SNP per gene based on pvalue
         tmp <- tmp %>% group_by(gene) %>% slice_min(pvalue, with_ties=FALSE)
         tmp <- tmp %>% arrange(gene) %>% ungroup() %>% select(pvalue)
@@ -57,10 +52,9 @@ for (cond in conditions){
     hist(best_true$qvals, main = cond%&%' '%&%ctype%&%' cisQTL qvalues')
     dev.off()
     
-    # qqplot with best true SNPs and best perm SNPs
-    best_perm <- data.frame(min_value = apply(compiled.perm, 1, min, na.rm = TRUE))
+    # qqplot with best true SNPs and best perm1 SNPs
     pdf('plots/'%&%cond%&%'_'%&%ctype%&%'_best_SNPs_qqplot.pdf', width=4, height=4)
-    qqplot(x=-log10(best_perm$min_value), y=-log10(best_true$pvalue), main = cond%&%' '%&%ctype%&%' Best SNPs qqplot', 
+    qqplot(x=-log10(compiled.perm[,1]), y=-log10(best_true$pvalue), main = cond%&%' '%&%ctype%&%' Best SNPs qqplot', 
            xlab='-log10(best permuted p-values)', ylab = '-log10(best true p-values)')
     abline(c(0,1), col='red')
     dev.off()
