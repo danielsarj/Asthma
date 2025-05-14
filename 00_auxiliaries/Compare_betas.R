74d716821cb0a00d544b82abf1ad88bbf174a7ea
danielsarj <dnsarj@gmail.com>
2025-05-14 10:58:30 -0500

    compare post mash effect sizes

--- 00_auxiliaries/Compare_betas.R
+++ 00_auxiliaries/Compare_betas.R
@@ -23,20 +23,21 @@ haley_cols <- gsub('CD8T', 'T-CD8', haley_cols)
 haley_cols <- gsub('CD4T', 'T-CD4', haley_cols)
 colnames(haley_b) <- haley_cols
 haley_snps <- haley_b$snps 
-haley_snps <- paste(haley_snps, collapse='|')
+
+daniel_b <- fread('mashr/mashr_out_beta_df.txt') %>% separate(snps, into=c('snpss','alt'), sep='_') %>% 
+  select(-c(alt, contains('RV'))) %>% rename(snps=snpss) 
 
 for (c in conditions){
   print(c)
   for (ct in celltypes){
     print(ct)
-    daniel_b <- fread('matrixEQTL_results/'%&%c%&%'_'%&%ct%&%'_elbowPCs_cisQTL_sumstats_V2.txt') %>% 
-      select(gene, snps, beta) %>% filter(grepl(haley_snps, snps))
 
-    a <- daniel_b %>% separate(snps, into=c('snpss','alt'), sep='_') %>% select(-snps) %>% rename(snps=snpss)
-    
+    daniel_subset <- daniel_b %>% select(gene, snps, c%&%'_'%&%ct%&%'_beta')
     haley_subset <- haley_b %>% select(gene, snps, ct%&%'_'%&%c)
-    haley_daniel_b <- inner_join(haley_subset, daniel_b, by=c('gene', 'snps')) %>%
+    
+    haley_daniel_b <- inner_join(haley_subset, daniel_subset, by=c('gene', 'snps')) %>%
       mutate(condition=c, celltype=ct)
+    
     colnames(haley_daniel_b)[3:4] <- c('H_beta', 'D_beta')
     
     if (exists('compiled.betas')){
@@ -51,5 +52,4 @@ ggplot(compiled.betas) + geom_pointdensity(aes(abs(D_beta), abs(H_beta)), show.l
   stat_smooth(aes(abs(D_beta), abs(H_beta)), method='lm', geom='smooth', formula=y~x) +
   geom_abline(slope=1, color='red') + theme_bw() + 
   facet_grid(cols=vars(celltype), rows=vars(condition)) + scale_color_viridis()
-ggsave('eQTLmapping_DanielvsHaley_betas_absbetas.pdf', height=5, width=10)
-
+ggsave('mashr/eQTLmapping_DanielvsHaley_betas_absbetas_postmash.pdf', height=5, width=10)
