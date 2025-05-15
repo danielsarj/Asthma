b6b0b9987595efe1a3ffe3bdfb848e9fc2a47184
danielsarj <dnsarj@gmail.com>
2025-05-15 13:50:51 -0500

    simplify scripts and generate more boxplots

--- 05_QTL_mapping/04c_compareMashrSignals.R
+++ 05_QTL_mapping/04c_compareMashrSignals.R
@@ -1,9 +1,14 @@
 library(tidyverse)
 library(data.table)
 library(patchwork)
+library(janitor)
 "%&%" <- function(a,b) paste(a,b, sep = "")
 setwd('/project/lbarreiro/USERS/daniel/asthma_project/QTLmapping/mashr')
 celltypes <- c('B', 'T-CD4', 'T-CD8', 'Mono', 'NK')
+conditions <- c('NI', 'RV', 'IVA')
+
+# load dosage file
+dos_matrix <- fread('../../genotypes/imputed_vcfs/imputed_dosage.txt')
 
 # read mashr dfs
 beta_df <- fread('mashr_out_beta_df.txt')
@@ -68,10 +73,124 @@ for (i in 1:length(celltypes)){
       ggsave(celltypes[i]%&%'_eGenes_forestplot.pdf', height=6, width=8)
       }
     
-    if(exists('final.df')){
-      final.df <- rbind(final.df, tmp)
-    } else {final.df <- tmp}
+    if(exists('sig_mashr')){
+      sig_mashr <- rbind(sig_mashr, tmp)
+    } else {sig_mashr <- tmp}
   }
 }
-final.df <- final.df %>% drop_na()
-fwrite(final.df, 'sig_mashr_results.lfsr0.05.txt', col.names=T, sep='\t')
+
+sig_mashr <- sig_mashr %>% drop_na() %>% select(gene, snps, celltype) %>% unique()
+# box plot
+for (i in 1:nrow(sig_mashr)){
+  print(i/nrow(sig_mashr)*100)
+  
+  # subset dosage file for the specific SNP
+  subset_dosage <- dos_matrix %>% filter(snpid==sig_mashr$snps[i]) %>% t() %>% 
+    as.data.frame() %>% rownames_to_column() %>% row_to_names(row_number=1) %>%
+    rename(ID=snpid)
+  
+  # get expression levels for the specific gene across all conditions
+  for (cond in conditions){
+    expression <- fread('../'%&%cond%&%'_'%&%sig_mashr$celltype[i]%&%'_elbowPCs.txt') %>%
+      filter(GENES==sig_mashr$gene[i]) %>% t() %>% as.data.frame() %>% rownames_to_column() %>% 
+      row_to_names(row_number=1) %>% rename(ID=GENES) %>% mutate(condition=cond)
+    
+    if (ncol(expression)==3){
+      if (exists('compiled.exp')){
+        compiled.exp <- rbind(compiled.exp, expression)
+      } else {compiled.exp <- expression}
+    }
+  }
+  
+  # join dosage and expression tbl
+  full_tbl <- full_join(subset_dosage, compiled.exp, by=c('ID')) %>% drop_na()
+  full_tbl[,2] <- as.factor(full_tbl[,2])
+  full_tbl[,3] <- as.numeric(full_tbl[,3])
+  full_tbl$condition <- factor(full_tbl$condition, levels=c('NI','IVA','RV'))
+  colnames(full_tbl)[2:3] <- c('SNP', 'Gene')
+  
+  # make boxplot
+  ggplot(full_tbl, aes(x=SNP, y=Gene)) + geom_boxplot() + 
+    xlab(sig_mashr$snps[i]) + ylab(sig_mashr$gene[i]) +
+    theme_bw() + facet_wrap(~condition) 
+  
+  # save plots
+  ggsave('QTL_boxplots/'%&%sig_mashr$celltype[i]%&%'_'%&%sig_mashr$gene[i]%&%'_QTL_boxplot.pdf', 
+         height=3, width=6)
+  
+  rm(compiled.exp)
+}
+
+# reformat dataframe 
+mash_df <- inner_join(beta_df, sd_df) %>% inner_join(lfsr_df)
+for (cond in c('NI','IVA','RV')){
+  for (ctype in celltypes){
+    sub <- mash_df %>% select(gene, snps, contains(cond%&%'_'%&%ctype)) %>% 
+      mutate(condition=cond, celltype=ctype)
+    colnames(sub) <- gsub(cond%&%'_'%&%ctype%&%'_', '', colnames(sub))
+    
+    if(exists('mash.long.df')){
+      mash.long.df <- rbind(mash.long.df, sub)
+    } else {mash.long.df <- sub}
+  }
+}
+
+# summary
+mash_summary <- mash.long.df %>% group_by(condition, celltype) %>% filter(lfsr < 0.05) %>% 
+  summarise(n_eGenes=n())
+ggplot(mash_summary, aes(x=celltype, y=n_eGenes, fill=condition)) + geom_col(position='dodge') +
+  theme_bw()
+ggsave('sig_eGenes_per_cond.ctype.pdf', height=4, width=5)
+
+# forest plot
+mash_filt <- mash.long.df %>% group_by(gene, snps, celltype) %>%
+  filter(any(lfsr<0.05) & any(lfsr>0.3)) %>%ungroup()
+mash_filt <- mash_filt %>% mutate(lower=beta-1.96*SD, upper=beta+1.96*SD)
+
+ggplot(mash_filt, aes(y=gene, x=beta, xmin=lower, xmax=upper, color=condition)) +
+  geom_pointrange(position=position_dodge(width=0.5), size=0.5) +
+  geom_vline(xintercept=0, linetype='dashed', color='black') + 
+  theme_bw() + labs(x='Effect Size', y='Gene', color='Condition') +
+  facet_wrap(~celltype)
+
+# box plots
+mash_filt <- mash_filt %>% select(gene, snps, celltype) %>% unique()
+for (i in 1:nrow(mash_filt)){
+  print(i/nrow(mash_filt)*100)
+  
+  # subset dosage file for the specific SNP
+  subset_dosage <- dos_matrix %>% filter(snpid==mash_filt$snps[i]) %>% t() %>% 
+    as.data.frame() %>% rownames_to_column() %>% row_to_names(row_number=1) %>%
+    rename(ID=snpid)
+  
+  # get expression levels for the specific gene across all conditions
+  for (cond in c('NI','IVA','RV')){
+    expression <- fread('../'%&%cond%&%'_'%&%mash_filt$celltype[i]%&%'_elbowPCs.txt') %>%
+      filter(GENES==mash_filt$gene[i]) %>% t() %>% as.data.frame() %>% rownames_to_column() %>% 
+      row_to_names(row_number=1) %>% rename(ID=GENES) %>% mutate(condition=cond)
+    
+    if (ncol(expression)==3){
+      if (exists('compiled.exp')){
+        compiled.exp <- rbind(compiled.exp, expression)
+      } else {compiled.exp <- expression}
+    }
+  }
+  
+  # join dosage and expression tbl
+  full_tbl <- full_join(subset_dosage, compiled.exp, by=c('ID')) %>% drop_na()
+  full_tbl[,2] <- as.factor(full_tbl[,2])
+  full_tbl[,3] <- as.numeric(full_tbl[,3])
+  full_tbl$condition <- factor(full_tbl$condition, levels=c('NI','IVA','RV'))
+  colnames(full_tbl)[2:3] <- c('SNP', 'Gene')
+  
+  # make boxplot
+  ggplot(full_tbl, aes(x=SNP, y=Gene)) + geom_boxplot() + 
+    xlab(mash_filt$snps[i]) + ylab(mash_filt$gene[i]) +
+    theme_bw() + facet_wrap(~condition) 
+  
+  # save plots
+  ggsave('QTL_boxplots/'%&%mash_filt$celltype[i]%&%'_'%&%mash_filt$gene[i]%&%'_QTL_boxplot.pdf', 
+         height=3, width=6)
+  
+  rm(compiled.exp)
+}
