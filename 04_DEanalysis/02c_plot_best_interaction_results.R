40ede945776d79e5bebe346c908bf25bfba53d4f
danielsarj <dnsarj@gmail.com>
2025-08-07 15:45:21 -0500

    use voom-transformed and adjusted expression levels in the boxplots

--- 04_DEanalysis/02c_plot_best_interaction_results.R
+++ 04_DEanalysis/02c_plot_best_interaction_results.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(data.table)
+library(edgeR)
+library(limma)
 library(reshape2)
 "%&%" <- function(a,b) paste(a,b, sep = "")
 setwd('/project/lbarreiro/USERS/daniel/asthma_project/DEanalysis')
@@ -33,12 +35,38 @@ for (int in interactions){
   
   # extract count matrix and keep only top genes
   count <- obj@assays$RNA$counts
-  count <- count[rownames(count) %in% top_genes$gene,] %>% as.data.frame()
-
+  
   for (cond in unique(top_genes$condition)){
     for (ct in unique(top_genes$celltype)){
+      # subset count object
+      sub_count <- count[,(grepl(cond, colnames(count)) | grepl('NI', colnames(count))) & grepl(ct, colnames(count))] 
       genes_to_look <- top_genes %>% filter(celltype==ct, condition==cond) %>% pull(gene)
-      sub_count <- count %>% select(contains(cond%&%'_'%&%ct), contains('NI_'%&%ct)) %>% rownames_to_column('gene') %>%
+      
+      # transform count into dge object
+      sub_count <- DGEList(counts=sub_count)
+      sub_count <- calcNormFactors(sub_count)
+      sub_count <- sub_count[genes_to_look, , keep.lib.sizes=TRUE]
+      
+      # subset metadata
+      sub_mdata <- mdata %>% rownames_to_column() %>% filter(rowname %in% rownames(sub_count$samples))
+      
+      # create design matrix (without effect of interest)
+      if (int=='none'){
+        design <- model.matrix(~IDs+batch+age+gender+n+avg_mt, data=sub_mdata)
+      } else if (int=='asthma_alb'){
+        design <- model.matrix(~age+gender+n+avg_mt+albuterol+condition, data=sub_mdata)
+      } else {
+        design <- model.matrix(~age+gender+n+avg_mt+condition, data=sub_mdata)
+      }
+      
+      # normalize and compute precision weights
+      v <- voom(sub_count, design)
+      
+      # access normalized expression
+      voom_logCPM <- v$E %>% as.data.frame()
+      
+      # filter by genes of interest
+      sub_count <- voom_logCPM %>% select(contains(cond%&%'_'%&%ct), contains('NI_'%&%ct)) %>% rownames_to_column('gene') %>%
         filter(gene %in% genes_to_look) %>% melt() %>% separate(variable, into=c('ID', 'condition', 'celltype'), sep='_')
       
       # merge with metadata
@@ -49,22 +77,19 @@ for (int in interactions){
         ggplot(sub_count, aes(x=condition, y=value)) + geom_boxplot() + theme_bw() +
           stat_compare_means(aes(group=condition), method='wilcox.test', label='p.format') +
           facet_wrap(~gene, scales='free')
+        ggsave('boxplots/top5_DE_'%&%int%&%'_'%&%cond%&%'_'%&%ct%&%'_boxplots.pdf', height=6, width=9)
         
-      } else if (int=='asthma'){
-        ggplot(sub_count, aes(x=condition, y=value, color=asthma)) + geom_boxplot() + theme_bw() +
-          stat_compare_means(aes(group = asthma), method='wilcox.test', label='p.format') +
-          facet_wrap(~gene, scales='free')   
-          
-      } else if (int=='asthma_alb'){
-        ggplot(sub_count, aes(x=condition, y=value, color=asthma)) + geom_boxplot() + theme_bw() +
-          stat_compare_means(aes(group = asthma), method='wilcox.test', label='p.format') +
-          facet_wrap(~gene, scales='free')         
+      } else if (int=='income'){
+        ggplot(sub_count, aes(x=condition, y=value, color=income)) + geom_boxplot() + theme_bw() +
+          stat_compare_means(aes(group = income), method='wilcox.test', label='p.format') +
+          facet_wrap(~gene, scales='free')
+        ggsave('boxplots/top5_DE_'%&%int%&%'_'%&%cond%&%'_'%&%ct%&%'_boxplots.pdf', height=6, width=9)
         
       } else {
-        ggplot(sub_count, aes(x=condition, y=value, color=income)) + geom_boxplot() + theme_bw() +
+        ggplot(sub_count, aes(x=condition, y=value, color=asthma)) + geom_boxplot() + theme_bw() +
           stat_compare_means(aes(group = asthma), method='wilcox.test', label='p.format') +
           facet_wrap(~gene, scales='free')
-        
+        ggsave('boxplots/top5_DE_'%&%int%&%'_'%&%cond%&%'_'%&%ct%&%'_boxplots.pdf', height=6, width=9)
       }
     }
   }
