cfaf6b3cf0dfe37dca4227e5c0a5143f8e1cbacb
danielsarj <dnsarj@gmail.com>
2025-08-11 16:29:37 -0500

    save final df

--- 00_auxiliaries/concatenate_SAIGE-QTL.R
+++ 00_auxiliaries/concatenate_SAIGE-QTL.R
@@ -3,19 +3,16 @@ library(data.table)
 "%&%" <- function(a,b) paste(a,b, sep = "")
 setwd('/project/lbarreiro/USERS/daniel/asthma_project/QTLmapping/Saige/step3/outputs/')
 celltypes <- c('NK_NI')
-pcs <- c('PCs1', 'PCs2')
+pcs <- c('PCs1', 'PCs2', 'PCs3', 'PCs4', 'PCs5')
 
 for (ct in celltypes){
   print(ct)
   for (pc in pcs){
+    print(pc)
     # find all SAIGE-QTL step3 outputs
     saige_files <- list.files(ct%&%'/exp_'%&%pc, full.names=T)
   
     for (f in saige_files){
-      # counter to keep track of progress
-      pct <- round((which(saige_files == f) / length(saige_files)) * 100)
-      if (pct %% 10 == 0) print(pct)
-      
       # read file
       tmp <- fread(f) %>% mutate(celltype=ct, exp_pcs=pc)
     
@@ -26,8 +23,8 @@ for (ct in celltypes){
     }
   }
 }
+fwrite(final_df, 'compiled_step3_outputs.txt', sep=' ', col.names=T)
 
 summary_df <- final_df %>% group_by(celltype, exp_pcs) %>% summarise(n_eGenes=sum(top_pval<1e-5))
-
 ggplot(summary_df, aes(x=exp_pcs, y=n_eGenes, group=celltype)) + geom_point() + geom_line() + theme_bw() +
   facet_wrap(~celltype)
