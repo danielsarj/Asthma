96e7f933f0bf02b4f08318eec470b523a4fcac7e
danielsarj <dnsarj@gmail.com>
2025-10-08 11:14:53 -0500

    implement CAMERA alongside GSEA

--- 04_DEanalysis/03_GSEA.R
+++ 04_DEanalysis/03_GSEA.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(data.table)
 library(fgsea)
 library(msigdbr)
+library(limma)
 '%&%' <- function(a,b) paste(a,b, sep = '')
 setwd('/project/lbarreiro/USERS/daniel/asthma_project/DEanalysis')
 conditions <- c('RV', 'IVA')
@@ -18,7 +19,8 @@ DE_results <- fread('NI_IVAxRV_integrated_limma_results.txt')
 for (int in interactions){
   for (i in 1:length(conditions)){
     for (ctype in cells_seurat){
-
+      
+      # GSEA  
       # subset based on interaction, celltype, and condition
       subset_DE_results <- DE_results %>% filter(celltype==ctype, condition==conditions[i], interaction==int) 
       
@@ -51,29 +53,54 @@ for (int in interactions){
         ggtitle('Enrichment Plot: HALLMARK_INTERFERON_GAMMA_RESPONSE')
       ggsave('NI_'%&%conditions[i]%&%'_'%&%ctype%&%'_'%&%int%&%'_INF_enrichmentplots.pdf', height=6, width=15)
       
+      # CAMERA
+      cam <- cameraPR(statistic=subset_DE_results, index=human.path.list) %>% rownames_to_column('pathway') %>%
+        mutate(celltype=ctype, condition=conditions[i], interaction=int) 
+      
       # compile results
       if (exists('compiled.fgseaRes')){
         compiled.fgseaRes <- rbind(compiled.fgseaRes, fgseaRes)
       } else {
         compiled.fgseaRes <- fgseaRes
       }
+      
+      # compile results
+      if (exists('compiled.cameraRes')){
+        compiled.cameraRes <- rbind(compiled.cameraRes, cam)
+      } else {
+        compiled.cameraRes <- cam
+      }
     }
   }
 }
-compiled.fgseaRes$pathway <- gsub('HALLMARK_', '', compiled.fgseaRes$pathway )
+compiled.fgseaRes$pathway <- gsub('HALLMARK_', '', compiled.fgseaRes$pathway)
 fwrite(compiled.fgseaRes, 'NI_IVAxRV_integrated_descGSEAresults.txt', sep=' ', na='NA', col.names=T)
+compiled.cameraRes$pathway <- gsub('HALLMARK_', '', compiled.cameraRes$pathway)
+fwrite(compiled.cameraRes, 'NI_IVAxRV_integrated_descCAMERAresults.txt', sep=' ', na='NA', col.names=T)
 
 # plot number of enriched pathways per celltype/condition/interaction
 summ <- compiled.fgseaRes %>% group_by(celltype, condition, interaction) %>% filter(padj<0.05) %>%
   summarise(n=n())
 summ$interaction <- factor(summ$interaction, levels=c('none','asthma','income'))
-ggplot(summ) + geom_col(aes(x=celltype, y=n)) + theme_bw() + 
+ggplot(summ) + geom_col(aes(x=celltype, y=n)) + theme_bw() + ylab('Number of enriched pathways') +
   facet_grid(cols=vars(interaction), rows=vars(condition))
 ggsave('NI_IVAxRV_descSigGeneSets_acrossInteractions.pdf', height=4, width=8)
 
-# bubble plots for significantly enriched pathways
+# filter CAMERA results by significance
+compiled.cameraRes <- compiled.cameraRes %>% filter(FDR<0.05)
+
+# filter GSEA results by CAMERA results
+joint_gsea_camera <- inner_join(compiled.cameraRes, compiled.fgseaRes, by=c('pathway', 'condition', 'interaction', 'celltype'))
+summ <- joint_gsea_camera %>% group_by(celltype, condition, interaction) %>% 
+  filter(padj<0.05, (Direction=='Up' & NES>0) | (Direction=='Down' & NES<0)) %>% summarise(n=n())
+summ$interaction <- factor(summ$interaction, levels=c('none','asthma','income'))
+ggplot(summ) + geom_col(aes(x=celltype, y=n)) + theme_bw() + ylab('Number of enriched pathways') +
+  facet_grid(cols=vars(interaction), rows=vars(condition))
+ggsave('NI_IVAxRV_descSigGeneSets_acrossInteractions_afterCAMERAfiltering.pdf', height=4, width=8)
+
+# bubble plots for significantly enriched pathways 
 for (int in interactions){
-  compiled.fgseaRes %>% filter(padj<0.05, interaction==int) %>% 
+  joint_gsea_camera %>% filter(padj<0.05, interaction==int) %>% 
     ggplot(., aes(x=NES, y=reorder(pathway, NES), size=size, color=-log10(padj))) +
     geom_point(alpha=0.8) + scale_size(range=c(1,10)) + scale_color_gradient(low='blue', high='red') +  
     labs(x='Normalized Enrichment Score (NES)', y='Pathway', size='Gene Count', color='-log10(padj)') +
