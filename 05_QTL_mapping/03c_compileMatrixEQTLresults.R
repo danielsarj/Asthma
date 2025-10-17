ecbe9d265f8a4cf73346aaf1b873d12b0f2ecb37
danielsarj <dnsarj@gmail.com>
2025-10-16 21:47:56 -0500

    compute qvalues for every pc

--- 05_QTL_mapping/03c_compileMatrixEQTLresults.R
+++ 05_QTL_mapping/03c_compileMatrixEQTLresults.R
@@ -7,58 +7,62 @@ setwd('/project/lbarreiro/USERS/daniel/asthma_project/QTLmapping/matrixEQTL_resu
 # define all vectors
 conditions <- c('NI', 'RV', 'IVA')
 celltypes <- c('B', 'T-CD4', 'T-CD8', 'Mono', 'NK')
+pcs <- c(1:20)
 permutations <- c(0:10)
 
 for (cond in conditions){
   print(cond)
   for (ctype in celltypes){
     print(ctype)
-    for (perm in permutations){
-      print(perm)
+    for (pc in pcs){
+      print(pc)
+      for (perm in permutations){
+        print(perm)
       
-      # select the top SNP for each gene in true and permuted files
-      if (perm==0){
-        best_true <- fread(cond%&%'_'%&%ctype%&%'_elbowPCs_cisQTL_sumstats.txt') 
+        # select the top SNP for each gene in true and permuted files
+        if (perm==0){
+          best_true <- fread(cond%&%'_'%&%ctype%&%'_'%&%pc%&%'PCs_cisQTL_sumstats.txt') 
         
-        #histogram of unadjusted pvalues
-        pdf('plots/'%&%cond%&%'_'%&%ctype%&%'_cisQTL_pvalhistogram.pdf', width=4, height=4)
-        hist(best_true$pvalue, main = cond%&%' '%&%ctype%&%' cisQTL pvalues', breaks=100)
-        dev.off()
+          #histogram of unadjusted pvalues
+          pdf('plots/'%&%cond%&%'_'%&%ctype%&%'_'%&%pc%&%'cisQTL_pvalhistogram.pdf', width=4, height=4)
+          hist(best_true$pvalue, main = cond%&%' '%&%ctype%&%' '%&%pc%&%' cisQTL pvalues', breaks=100)
+          dev.off()
         
-        # select top SNP per gene based on pvalue
-        best_true <- best_true %>% group_by(gene) %>% slice_min(pvalue, with_ties=FALSE)
-        best_true <- best_true %>% arrange(gene)
+          # select top SNP per gene based on pvalue
+          best_true <- best_true %>% group_by(gene) %>% slice_min(pvalue, with_ties=FALSE)
+          best_true <- best_true %>% arrange(gene)
         
-      } else {
-        tmp <- fread(cond%&%'_'%&%ctype%&%'_Perm'%&%perm%&%'_elbowPCs_cisQTL_sumstats.txt') 
+        } else {
+          tmp <- fread(cond%&%'_'%&%ctype%&%'_Perm'%&%perm%&%'_'%&%pc%&%'PCs_cisQTL_sumstats.txt') 
         
-        # select top SNP per gene based on pvalue
-        tmp <- tmp %>% group_by(gene) %>% slice_min(pvalue, with_ties=FALSE)
-        tmp <- tmp %>% arrange(gene) %>% ungroup() %>% select(pvalue)
+          # select top SNP per gene based on pvalue
+          tmp <- tmp %>% group_by(gene) %>% slice_min(pvalue, with_ties=FALSE)
+          tmp <- tmp %>% arrange(gene) %>% ungroup() %>% select(pvalue)
         
-        if (exists('compiled.perm')){
-          compiled.perm <- cbind(compiled.perm, tmp)
-        } else {compiled.perm <- tmp}
+          if (exists('compiled.perm')){
+            compiled.perm <- cbind(compiled.perm, tmp)
+          } else {compiled.perm <- tmp}
+        }
       }
+    
+      # compute qvalues with the top SNPs
+      empP <- empPvals(stat=-log10(best_true$pvalue), stat0=-log10(as.matrix(compiled.perm)), pool=TRUE)
+      best_true$qvals <- qvalue(empP)$qvalue
+      fwrite(best_true, cond%&%'_'%&%ctype%&%'_'%&%pc%&%'_best_cisQTL_sumstats.txt', sep=' ')
+    
+      #histogram of qvalues
+      pdf('plots/'%&%cond%&%'_'%&%ctype%&%'_'%&%pc%&%'_cisQTL_qvalhistogram.pdf', width=4, height=4)
+      hist(best_true$qvals, main = cond%&%' '%&%ctype%&%' '%&%pc%&%' cisQTL qvalues')
+      dev.off()
+    
+      # qqplot with best true SNPs and best perm1 SNPs
+      pdf('plots/'%&%cond%&%'_'%&%ctype%&%'_'%&%pc%&%'_best_SNPs_qqplot.pdf', width=4, height=4)
+      qqplot(x=-log10(compiled.perm[,1]), y=-log10(best_true$pvalue), main = cond%&%' '%&%ctype%&%' '%&%pc%&%' Best SNPs qqplot', 
+            xlab='-log10(best permuted p-values)', ylab = '-log10(best true p-values)')
+      abline(c(0,1), col='red')
+      dev.off()
+    
+      rm(compiled.perm)
     }
-    
-    # compute qvalues with the top SNPs
-    empP <- empPvals(stat=-log10(best_true$pvalue), stat0=-log10(as.matrix(compiled.perm)), pool=TRUE)
-    best_true$qvals <- qvalue(empP)$qvalue
-    fwrite(best_true, cond%&%'_'%&%ctype%&%'_best_cisQTL_sumstats.txt', sep=' ')
-    
-    #histogram of qvalues
-    pdf('plots/'%&%cond%&%'_'%&%ctype%&%'_cisQTL_qvalhistogram.pdf', width=4, height=4)
-    hist(best_true$qvals, main = cond%&%' '%&%ctype%&%' cisQTL qvalues')
-    dev.off()
-    
-    # qqplot with best true SNPs and best perm1 SNPs
-    pdf('plots/'%&%cond%&%'_'%&%ctype%&%'_best_SNPs_qqplot.pdf', width=4, height=4)
-    qqplot(x=-log10(compiled.perm[,1]), y=-log10(best_true$pvalue), main = cond%&%' '%&%ctype%&%' Best SNPs qqplot', 
-           xlab='-log10(best permuted p-values)', ylab = '-log10(best true p-values)')
-    abline(c(0,1), col='red')
-    dev.off()
-    
-    rm(compiled.perm)
   }
 }
