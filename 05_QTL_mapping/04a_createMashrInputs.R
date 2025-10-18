6d3562600c4d613f9cb078ed959571e4f72dded3
danielsarj <dnsarj@gmail.com>
2025-10-17 20:59:16 -0500

    create mashr inputs based on matrixeqtl results with the best pc

--- 05_QTL_mapping/04a_createMashrInputs.R
+++ 05_QTL_mapping/04a_createMashrInputs.R
@@ -6,81 +6,72 @@ setwd('/project/lbarreiro/USERS/daniel/asthma_project/QTLmapping/matrixEQTL_resu
 # define all vectors
 conditions <- c('NI', 'RV', 'IVA')
 celltypes <- c('B', 'T-CD4', 'T-CD8', 'Mono', 'NK')
+input_prefix <- c('IVA_B_7', 'NI_B_7', 'RV_B_16', 'IVA_Mono_1', 'NI_Mono_2', 'RV_Mono_1', 
+                  'IVA_NK_2', 'NI_NK_3', 'RV_NK_4', 'IVA_T-CD4_3', 'NI_T-CD4_2', 'RV_T-CD4_10',
+                  'IVA_T-CD8_1', 'NI_T-CD8_6', 'RV_T-CD8_7')
 
 # get gene intersection
-for (cond in conditions){
-  for (ctype in celltypes){
-    tmp <- fread('../'%&%cond%&%'_'%&%ctype%&%'_elbowPCs.txt') %>% pull(GENES)
-    if (exists('shared_genes')){
-      shared_genes <- intersect(shared_genes, tmp)
-    } else {shared_genes <- tmp}
-  }
+for (f in input_prefix){
+  tmp <- fread('../'%&%f%&%'PCs.txt') %>% pull(GENES)
+  if (exists('shared_genes')){
+    shared_genes <- intersect(shared_genes, tmp)
+  } else {shared_genes <- tmp}
 }
 
 # retrieve cis-snps pairs that are shared across all conditions/celltypes
-for (cond in conditions){
-  print(cond)
-  for (ctype in celltypes){
-    print(ctype)
-    tmp <- fread(cond%&%'_'%&%ctype%&%'_elbowPCs_cisQTL_sumstats.txt') %>% 
-      filter(gene %in% shared_genes) %>% select(gene, snps)
-      
-    if (exists('combined_snps')){
-      combined_snps <- intersect(combined_snps, tmp)
-    } else {combined_snps <- tmp}
-  }
+for (f in input_prefix){
+  tmp <- fread(f%&%'PCs_cisQTL_sumstats.txt') %>% 
+    filter(gene %in% shared_genes) %>% select(gene, snps)
+    
+  if (exists('combined_snps')){
+    combined_snps <- intersect(combined_snps, tmp)
+  } else {combined_snps <- tmp}
 }
 fwrite(combined_snps, 'intersection_cisQTL.txt', sep='\t', quote=F)
-rm(shared_genes, tmp)
+rm(tmp)
 
 # get the most significant (lowest qvalue) cis-snps across all conditions/celltypes
-for (cond in conditions){
-  print(cond)
-  for (ctype in celltypes){
-    print(ctype)
-    tmp <- fread(cond%&%'_'%&%ctype%&%'_best_cisQTL_sumstats.txt') %>% 
-      select(gene, snps, qvals) 
+for (f in input_prefix){
+  tmp <- fread(f%&%'_best_cisQTL_sumstats.txt') %>% 
+    filter(gene %in% shared_genes) %>% select(gene, snps, qvals) 
     
-    if (exists('top_pairs')){
-      # combine both data frames
-      top_pairs <- rbind(top_pairs, tmp)
+  if (exists('top_pairs')){
+    # combine both data frames
+    top_pairs <- rbind(top_pairs, tmp)
       
-      # a gene should only be in the df once (lowest qval)
-      top_pairs <- top_pairs %>% group_by(gene) %>%
-        slice_min(qvals, with_ties=F) %>% ungroup()
-      
-    } else {top_pairs <- tmp}
-  }
+    # a gene should only be in the df once (lowest qval)
+    top_pairs <- top_pairs %>% group_by(gene) %>%
+      slice_min(qvals, with_ties=F) %>% ungroup()
+    
+  } else {top_pairs <- tmp}
 }
-rm(tmp)
+rm(shared_genes, tmp)
 top_pairs <- top_pairs %>% select(gene, snps)
 
 # get betas/SEs for each random cis-snps pair and for each top cis-snps pair for every conditions/celltypes
 combined_snps <- combined_snps %>% slice_sample(n=200000)
-for (cond in conditions){
-  print(cond)
-  for (ctype in celltypes){
-    print(ctype)
+for (f in input_prefix){
+  base <- sub('_[^_]+$', '', f)
+  
+  tmp <- fread(f%&%'PCs_cisQTL_sumstats.txt') %>% 
+    select(gene, snps, beta, SE) 
     
-    tmp <- fread(cond%&%'_'%&%ctype%&%'_elbowPCs_cisQTL_sumstats.txt') %>% 
-      select(gene, snps, beta, SE) 
+  tmp_random <- tmp %>% right_join(combined_snps, by=c('gene', 'snps')) 
+  colnames(tmp_random)[3:4] <- c(base%&%'_beta', base%&%'_SE')
+
+  tmp_strong <- tmp %>% right_join(top_pairs, by=c('gene', 'snps'))
+  colnames(tmp_strong)[3:4] <- c(base%&%'_beta', base%&%'_SE')
     
-    tmp_random <- tmp %>% right_join(combined_snps, by=c('gene', 'snps')) %>%
-      group_by(gene, snps) %>% slice_head(n=1)
-    colnames(tmp_random)[3:4] <- c(cond%&%'_'%&%ctype%&%'_beta', cond%&%'_'%&%ctype%&%'_SE')
-    
-    tmp_strong <- tmp %>% right_join(top_pairs, by=c('gene', 'snps'))
-    colnames(tmp_strong)[3:4] <- c(cond%&%'_'%&%ctype%&%'_beta', cond%&%'_'%&%ctype%&%'_SE')
-    
-    if (exists('random_df')){
-      random_df <- left_join(random_df, tmp_random, by=c('gene', 'snps'))
-    } else {random_df <- tmp_random}
-    
-    if (exists('strong_df')){
-      strong_df <- left_join(strong_df, tmp_strong, by=c('gene', 'snps'))
-    } else {strong_df <- tmp_strong}
-    
-  }
+  if (exists('random_df')){
+    random_df <- left_join(random_df, tmp_random, by=c('gene', 'snps'))
+  } else {random_df <- tmp_random}
+  
+  if (exists('strong_df')){
+    strong_df <- left_join(strong_df, tmp_strong, by=c('gene', 'snps'))
+  } else {strong_df <- tmp_strong}
 }
+random_df <- random_df %>% select(gene, snps, contains('NI'), contains('RV'), contains('IVA'))
+strong_df <- strong_df %>% select(gene, snps, contains('NI'), contains('RV'), contains('IVA'))
+
 fwrite(random_df, '../mashr/mashr_in_random_df.txt', quote=F, sep='\t', na='NA')
 fwrite(strong_df, '../mashr/mashr_in_strong_df.txt', quote=F, sep='\t', na='NA')
