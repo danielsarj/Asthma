f8e9ad8a3bd6869e3bc2d388f8157835ee0b672d
danielsarj <dnsarj@gmail.com>
2025-10-23 05:27:15 -0500

    change partition to caslake

--- 05_QTL_mapping/05b_coloc_wrapper.R
+++ 05_QTL_mapping/05b_coloc_wrapper.R
@@ -9,7 +9,7 @@ gwas_files <- c('FerreiraMAR_COA.h.tsv.gz', 'SakaueS_COA.h.tsv.gz')
 
 # sbtach file topper
 sbatch_topper <- '#!/bin/sh\n' %&% '#SBATCH --time=20:00:00\n' %&%
-  '#SBATCH --mem=50G\n' %&% '#SBATCH --partition=lbarreiro\n' %&%
+  '#SBATCH --mem=50G\n' %&% '#SBATCH --partition=caslake\n' %&%
   '#SBATCH --account=pi-lbarreiro\n\n' %&% 'conda init\n' %&%
   'conda activate seurat_env\n\n' %&%
   'export R_LIBS_USER=$CONDA_PREFIX/lib/R/library\n' %&%
