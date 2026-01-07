2497af5d6d9b9beb64ee7a88768ec0798404ebd4
danielsarj <dnsarj@gmail.com>
2026-01-07 17:08:04 -0600

    add 0 PC

--- 05_QTL_mapping/03b_MatrixEQTL_wrapper.R
+++ 05_QTL_mapping/03b_MatrixEQTL_wrapper.R
@@ -3,8 +3,8 @@ setwd('/project/lbarreiro/USERS/daniel/asthma_project/QTLmapping/jobs')
 
 # arguments
 conditions <- c('NI', 'RV', 'IVA')
-celltypes <- c('B', 'T-CD4', 'T-CD8', 'Mono', 'NK')
-pcs <- c(1:20)
+celltypes <- c('B', 'CD4-T', 'CD8-T', 'Mono', 'NK')
+pcs <- c(0:20)
 
 # sbtach file topper
 sbatch_topper <- '#!/bin/sh\n' %&% '#SBATCH --time=2:00:00\n' %&%
