453cc15b5f92a1622dcde23955798763e2ed802e
danielsarj <dnsarj@gmail.com>
2026-02-12 10:06:07 -0600

    update input prefix based on newly adjusted exp matrixeqtl results

--- 05_QTL_mapping/05b_coloc_wrapper.R
+++ 05_QTL_mapping/05b_coloc_wrapper.R
@@ -2,21 +2,21 @@
 setwd('/project/lbarreiro/USERS/daniel/asthma_project/QTLmapping/colocalization/jobs')
 
 # arguments
-input_prefix <- c('IVA_B_4',
-                  'NI_B_5',
-                  'RV_B_4',
+input_prefix <- c('IVA_B_2',
+                  'NI_B_0',
+                  'RV_B_0',
                   'IVA_CD4-T_0',
                   'NI_CD4-T_0',
                   'RV_CD4-T_1',
-                  'IVA_CD8-T_1',
+                  'IVA_CD8-T_2',
                   'NI_CD8-T_0',
-                  'RV_CD8-T_2',
-                  'IVA_Mono_14',
-                  'NI_Mono_19',
+                  'RV_CD8-T_1',
+                  'IVA_Mono_19',
+                  'NI_Mono_20',
                   'RV_Mono_0',
-                  'IVA_NK_0',
-                  'NI_NK_2',
-                  'RV_NK_0')
+                  'IVA_NK_2',
+                  'NI_NK_9',
+                  'RV_NK_5')
 gwas_files <- c('FerreiraMAR_COA.h.tsv.gz', 'SakaueS_COA.h.tsv.gz')
 
 # sbtach file topper
